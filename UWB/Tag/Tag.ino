f42958ad03f39674de65e66cce7722f40977b4b4
Ahmed Al-Tuwaijari <76178825+Ahmedaltu@users.noreply.github.com>
2023-07-02 16:08:14 +0300

    Add files via upload

---
+++ UWB/Tag/Tag.ino
@@ -0,0 +1,74 @@
+#include <SPI.h>
+#include <DW1000Ranging.h>
+
+#include "link.h"
+
+#define SPI_SCK 18
+#define SPI_MISO 19
+#define SPI_MOSI 23
+#define DW_CS 4
+#define PIN_RST 27
+#define PIN_IRQ 34
+
+struct MyLink *uwb_data;
+int index_num = 0;
+long runtime = 0;
+String all_json = "";
+
+void setup()
+{
+    //Serial.begin(115200);
+    
+    delay(1000);
+
+    //init the configuration
+    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+    DW1000Ranging.initCommunication(PIN_RST, DW_CS, PIN_IRQ);
+    DW1000Ranging.attachNewRange(newRange);
+    DW1000Ranging.attachNewDevice(newDevice);
+    DW1000Ranging.attachInactiveDevice(inactiveDevice);
+
+    //we start the module as a tag
+    DW1000Ranging.startAsTag("7D:00:22:EA:82:60:3B:9C", DW1000.MODE_LONGDATA_RANGE_LOWPOWER);
+
+    uwb_data = init_link();
+}
+
+void loop()
+{
+    DW1000Ranging.loop();
+
+}
+
+void newRange()
+{
+    char c[30];
+
+    //Serial.print("from: ");
+    //Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+    //Serial.print("\t Range: ");
+    //Serial.print(DW1000Ranging.getDistantDevice()->getRange());
+    //Serial.print(" m");
+    //Serial.print("\t RX power: ");
+    //Serial.print(DW1000Ranging.getDistantDevice()->getRXPower());
+    //Serial.println(" dBm");
+    fresh_link(uwb_data, DW1000Ranging.getDistantDevice()->getShortAddress(), DW1000Ranging.getDistantDevice()->getRange(), DW1000Ranging.getDistantDevice()->getRXPower());
+}
+
+void newDevice(DW1000Device *device)
+{
+    //Serial.print("ranging init; 1 device added ! -> ");
+    //Serial.print(" short:");
+    //Serial.println(device->getShortAddress(), HEX);
+
+    add_link(uwb_data, device->getShortAddress());
+}
+
+void inactiveDevice(DW1000Device *device)
+{
+    //Serial.print("delete inactive device: ");
+    //Serial.println(device->getShortAddress(), HEX);
+
+    delete_link(uwb_data, device->getShortAddress());
+}
+
